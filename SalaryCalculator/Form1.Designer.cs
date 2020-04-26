b1d46a4b8e3b1d7d052333b5f180dfa28ab16b5e
Nick Rentschler <nickrench3@icloud.com>
2020-04-26 12:37:46 -0500

    Add project files.

---
+++ SalaryCalculator/Form1.Designer.cs
@@ -0,0 +1,142 @@
+﻿namespace SalaryCalculator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.hourlyTextBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.salaryTextBox = new System.Windows.Forms.TextBox();
+            this.enterButton = new System.Windows.Forms.Button();
+            this.clrButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // hourlyTextBox
+            // 
+            this.hourlyTextBox.Location = new System.Drawing.Point(444, 125);
+            this.hourlyTextBox.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.hourlyTextBox.Name = "hourlyTextBox";
+            this.hourlyTextBox.Size = new System.Drawing.Size(196, 31);
+            this.hourlyTextBox.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(124, 138);
+            this.label1.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(252, 25);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "What is your hourly rate?";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.label2.Location = new System.Drawing.Point(246, 54);
+            this.label2.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(128, 20);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Salary Calculator";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(124, 323);
+            this.label3.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(221, 25);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Your annual salary is:";
+            // 
+            // salaryTextBox
+            // 
+            this.salaryTextBox.Location = new System.Drawing.Point(444, 310);
+            this.salaryTextBox.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.salaryTextBox.Name = "salaryTextBox";
+            this.salaryTextBox.ReadOnly = true;
+            this.salaryTextBox.Size = new System.Drawing.Size(196, 31);
+            this.salaryTextBox.TabIndex = 3;
+            // 
+            // enterButton
+            // 
+            this.enterButton.Location = new System.Drawing.Point(444, 217);
+            this.enterButton.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.enterButton.Name = "enterButton";
+            this.enterButton.Size = new System.Drawing.Size(172, 44);
+            this.enterButton.TabIndex = 1;
+            this.enterButton.Text = "Enter";
+            this.enterButton.UseVisualStyleBackColor = true;
+            this.enterButton.Click += new System.EventHandler(this.enterButton_Click);
+            // 
+            // clrButton
+            // 
+            this.clrButton.Location = new System.Drawing.Point(200, 217);
+            this.clrButton.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.clrButton.Name = "clrButton";
+            this.clrButton.Size = new System.Drawing.Size(172, 44);
+            this.clrButton.TabIndex = 4;
+            this.clrButton.Text = "Clear";
+            this.clrButton.UseVisualStyleBackColor = true;
+            this.clrButton.Click += new System.EventHandler(this.clrButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(820, 477);
+            this.Controls.Add(this.clrButton);
+            this.Controls.Add(this.enterButton);
+            this.Controls.Add(this.salaryTextBox);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.hourlyTextBox);
+            this.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Salary Calculator";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox hourlyTextBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox salaryTextBox;
+        private System.Windows.Forms.Button enterButton;
+        private System.Windows.Forms.Button clrButton;
+    }
+}
+
---
+++ SalaryCalculator/Form1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SalaryCalculator
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void enterButton_Click(object sender, EventArgs e)
+        {
+            double hourlyRate = Convert.ToDouble(hourlyTextBox.Text);
+            double annualSalary = hourlyRate * 2080;
+
+
+            salaryTextBox.Text = annualSalary.ToString();
+        }
+
+        private void clrButton_Click(object sender, EventArgs e)
+        {
+            hourlyTextBox.Clear();
+            salaryTextBox.Clear();
+        }
+    }
+}
